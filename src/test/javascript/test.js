0417b9d1fd8c4147ef80972594f9559c6db3c936
pcummuskey <pcummuskey@bravurasolutions.com>
2014-07-23 15:55:37 +1200

    Actually adding a useful test method.

--- src/test/javascript/test.js
+++ src/test/javascript/test.js
@@ -1,5 +1,8 @@
 goog.require('goog.testing.jsunit');
+goog.require('nz.co.aetheric.angular.AngularHelpers');
 
-testTrueIsTrue = function() {
-	assertTrue(true);
+testFunctionsAreDefined = function() {
+	assertNotUndefined('Function angular.controller should be defined.', angular.controller);
+	assertNotUndefined('Function angular.service should be defined.', angular.service);
+	assertNotUndefined('Function angular.directive should be defined.', angular.directive);
 };
